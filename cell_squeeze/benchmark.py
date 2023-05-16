b75b413ccdb4efd33a05e115212b0c9aaf1c1e9c
maharshi95 <mgor@cs.umd.edu>
2023-05-16 04:10:20 -0400

    Refactored some sparse goop code

--- cell_squeeze/benchmark.py
+++ cell_squeeze/benchmark.py
@@ -1,5 +1,6 @@
 # %%
 
+from bz2 import compress
 import os
 import numpy as np
 from sklearn.cluster import SpectralBiclustering
@@ -13,6 +14,7 @@ from scipy.sparse import csr_matrix, csc_matrix, coo_matrix, save_npz, load_npz
 from utils import flatten_array_dict, unflatten_array_dict
 from tqdm import tqdm
 from rich import print as rprint
+from typing import Any
 
 # %%
 permuter = BiClusterMatrixPermuter(2, 2)
@@ -26,40 +28,55 @@ mat = np.array(load_npz(data_path).todense())
 # %%
 
 
-def make_int_vector_goop(mat: np.ndarray, bitwidth: int = 32):
-    shape = mat.shape
+def array2intvec(mat: np.ndarray, bitwidth: int = 32) -> tuple[IntVector, tuple[int]]:
     int_vec = IntVector(mat.size, bitwidth)
     for i, elem in enumerate(mat.reshape(-1)):
         int_vec[i] = elem
-    return {
-        "data": int_vec,
-        "shape": shape,
-    }
+    return int_vec, mat.shape
 
 
-def make_array_from_int_vector_goop(goop: dict[str, tuple[int] | IntVector]):
+def intvec2nparray(intvec: IntVector, shape: tuple[int]) -> np.ndarray:
     """De-serializes an IntVector goop dict into a numpy array"""
-    shape = goop["shape"]
-    data = goop["data"]
-    array = data.to_numpy()
+    array = intvec.to_numpy()
     N = np.prod(shape)
-    array = array[-N:]
-    return array.reshape(shape)
+    return array[-N:].reshape(shape)
 
 
-def make_sparse_goop(mat: np.ndarray, method: str = "csr"):
+def mat_to_sparse_goop(mat: np.ndarray, method: str = "csr"):
     if method == "csr":
         sparse_mat = csr_matrix(mat)
     else:
         raise NotImplementedError
     return {
-        "d": make_int_vector_goop(sparse_mat.data)["data"],
-        "i1": make_int_vector_goop(sparse_mat.indices)["data"],
-        "i2": make_int_vector_goop(sparse_mat.indptr)["data"],
+        "d": array2intvec(sparse_mat.data)[0],
+        "i1": array2intvec(sparse_mat.indices)[0],
+        "i2": array2intvec(sparse_mat.indptr)[0],
         "n": sparse_mat.shape[0],
         "w": sparse_mat.nnz,
     }
 
+
+def sparse_goop_to_mat(sparse_goop: dict[str, Any], method: str = "csr"):
+    data = intvec2nparray(sparse_goop["d"], (sparse_goop["w"],))
+    indices = intvec2nparray(sparse_goop["i1"], (sparse_goop["w"],))
+    indptr = intvec2nparray(sparse_goop["i2"], (sparse_goop["n"] + 1,))
+    if method == "csr":
+        return csr_matrix((data, indices, indptr)).todense()
+    else:
+        raise NotImplementedError
+
+
+np.random.seed(0)
+R = np.random.rand(31, 23)
+R = (R > 0.8).astype(int) * np.random.randint(0, 10, size=(31, 23))
+Rs = csr_matrix(R)
+print(Rs.data.shape, Rs.indices.shape, Rs.indptr.shape)
+iv = array2intvec(R)[0]
+R2 = intvec2nparray(iv, R.shape)
+print(np.allclose(R, R2))
+g = mat_to_sparse_goop(R)
+R2 = sparse_goop_to_mat(g)
+np.allclose(R, R2)
 
 # %%
 
@@ -97,45 +114,41 @@ def make_goop_dict(mat: np.ndarray):
         cluster_mins.append(cluster_min)
 
         cluster_deltas = cluster_mat - cluster_min
-        sparse_goop = make_sparse_goop(cluster_deltas)
+        sparse_goop = mat_to_sparse_goop(cluster_deltas)
         sparse_goops.append(sparse_goop)
 
-    cluster_mins_goop = make_int_vector_goop(np.array(cluster_mins))
-    goop_dict["m"] = cluster_mins_goop["data"]
-    goop_dict["r"] = make_int_vector_goop(np.array(row_labels))["data"]
-    goop_dict["c"] = make_int_vector_goop(np.array(col_labels))["data"]
+    goop_dict["m"] = array2intvec(np.array(cluster_mins))[0]
+    goop_dict["r"] = array2intvec(np.array(row_labels))[0]
+    goop_dict["c"] = array2intvec(np.array(col_labels))[0]
     goop_dict["sh"] = (len(row_labels), len(col_labels))
     goop_dict["s"] = sparse_goops
     return goop_dict
 
 
 def ungoop_data(goop_dict: dict[str, tuple[int] | IntVector]):
-    shape = goop_dict["sh"]
+    n_rows, n_cols = goop_dict["sh"]
     sparse_goops = goop_dict["s"]
-    mat = np.zeros(shape)
+    mat = np.zeros(shape=(n_rows, n_cols))
 
-    cluster_mins = make_array_from_int_vector_goop(
-        {
-            "data": IntVector.from_bitword_array(goop_dict["m"], 32),
-            "shape": (len(goop_dict["m"]), 1),
-        }
+    row_labels = intvec2nparray(
+        IntVector.from_bitword_array(goop_dict["r"], 32),
+        (n_rows,),
     )
 
-    row_labels = make_array_from_int_vector_goop(
-        {
-            "data": IntVector.from_bitword_array(goop_dict["r"], 32),
-            "shape": (sparse_goops[0]["n"], 1),
-        }
-    )
-    col_labels = make_array_from_int_vector_goop(
-        {
-            "data": IntVector.from_bitword_array(goop_dict["c"], 32),
-            "shape": (sparse_goops[0]["w"], 1),
-        }
+    col_labels = intvec2nparray(
+        IntVector.from_bitword_array(goop_dict["c"], 32),
+        (n_cols,),
     )
 
     n_row_clusters = len(np.unique(row_labels))
     n_col_clusters = len(np.unique(col_labels))
+    n_clusters = n_row_clusters * n_col_clusters
+
+    cluster_mins = intvec2nparray(
+        IntVector.from_bitword_array(goop_dict["m"], 32),
+        (n_clusters,),
+    )
+
     clusters = list(product(range(n_row_clusters), range(n_col_clusters)))
     for l_row, l_col in tqdm(clusters):
         c_idx = l_row * len(np.unique(col_labels)) + l_col
@@ -143,12 +156,12 @@ def ungoop_data(goop_dict: dict[str, tuple[int] | IntVector]):
         cluster_cols = np.where(col_labels == l_col)[0]
         cluster_mat = mat[cluster_rows, :][:, cluster_cols]
         cluster_min = cluster_mins[c_idx]
-        cluster_deltas = make_array_from_int_vector_goop(
-            {
-                "data": IntVector.from_bitword_array(sparse_goops[c_idx]["d"], 32),
-                "shape": (sparse_goops[c_idx]["n"], sparse_goops[c_idx]["w"]),
-            }
-        )
+        intvec_goop = {
+            k: IntVector.from_bitword_array(v, 32) if k in {"d", "i1", "i2"} else v
+            for k, v in sparse_goops[c_idx].items()
+        }
+        print(intvec_goop)
+        cluster_deltas = sparse_goop_to_mat(intvec_goop, method="csr")
         cluster_mat = cluster_deltas + cluster_min
         mat[cluster_rows, :][:, cluster_cols] = cluster_mat
     return mat
@@ -160,12 +173,13 @@ goop_dict_flat = flatten_array_dict(goop_dict)
 rprint(goop_dict_flat.keys())
 
 # %%
-np.savez_compressed("/tmp/goop.npz", **goop_dict_flat)
+np.savez_compressed("/tmp/goop.npz", **goop_dict_flat, compressed=True)
 goop_size = os.path.getsize("/tmp/goop.npz")
 print("Goop size:", goop_size)
 
 gooped_bits = {**np.load("/tmp/goop.npz")}
 gooped_bits_unflat = unflatten_array_dict(gooped_bits)
+print(gooped_bits_unflat)
 mat_recon = ungoop_data(gooped_bits_unflat)
 print(np.allclose(mat, mat_recon))
 
